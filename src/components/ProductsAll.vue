321ff7b3ed3dd86f58ee85bf3b24d59b05295e6e
Wojciech Peisert <wojtekp@sauron.pl>
2022-07-04 22:58:22 +0200

    order management

--- src/components/ProductsAll.vue
+++ src/components/ProductsAll.vue
@@ -1,19 +1,36 @@
 <template>
   <div v-if="!loaded"><h1>LOADING !!!</h1></div>
-  <section class="categories">
-    <input type="button" :value="category.name" v-for="category in categories" v-bind:key="category.id"
-           @click="showProducts(category.id)" class="category-button"
-           :class="{'category-selected': category.id === category_id}"
-    />
-  </section>
+  <div class="main">
+    <div class="categories">
+      <input type="button" :value="category.name" v-for="category in categories" v-bind:key="category.id"
+             @click="showProducts(category.id)" class="category-button"
+             :class="{'category-selected': category.id === category_id}"
+      />
+    </div>
+    <div class="order-count" :class="{'order-hidden': getOrderCount() === 0}">
+      {{ getOrderCount() }} &nbsp; &nbsp; &nbsp;
+    </div>
+    <div class="order-count" :class="{'order-hidden': getOrderCount() === 0}">
+      {{ getOrderTotal() }} zł
+    </div>
+    <div style="clear: both;"></div>
+  </div>
   <section class="products">
-    <div class="product" v-for="product in products" v-bind:key="product.id">
+    <div class="product" v-for="product in products" v-bind:key="product.id" @click="clickProduct(product.id)">
       <div class="product-image-wrapper">
         <img class="product-image" v-bind:src="'/products/images/' + product.image" alt="image">
       </div>
       <div class="product-content">
-        <h4 class="product-name">{{ product.name }}</h4>
-        <p class="product-description">{{ product.description }}</p>
+        <div class="product-name">{{ product.name }}</div>
+        <div class="product-price">{{ product.price }} zł</div>
+        <div style="clear: both;"></div>
+        <div class="product-description">{{ product.description }}</div>
+        <div class="product-order" :class="{'order-hidden': getOrderProductCount(product.id) === 0}">
+          <input type="button" class="product-minus" value="-" v-on:click.stop="decrProduct(product.id)"/>
+          <input type="button" class="product-count" :value="getOrderProductCount(product.id)" />
+          <input type="button" class="product-plus" value="+" v-on:click.stop="incrProduct(product.id)"/>
+        </div>
+        <div style="clear: both;"></div>
       </div>
     </div>
     
@@ -32,7 +49,8 @@ export default {
       categories: [],
       products: [],
       loaded: false,
-      category_id: 0
+      category_id: 0,
+      order: { products: []}
     }
   },
   mounted () {
@@ -66,6 +84,50 @@ export default {
             console.log(error);
             alert(error);
           });
+    },
+    getOrderProductCount(productId) {
+      if (!this.order || !this.order.products || !this.order.products[productId]) {
+        return 0;
+      }
+      return this.order.products[productId];
+    },
+    clickProduct(productId) {
+      if (this.order.products[productId]) {
+        return;
+      }
+      this.order.products[productId] = 1;
+    },
+    incrProduct(productId) {
+      this.order.products[productId]++;
+    },
+    decrProduct(productId) {
+      this.order.products[productId]--;
+    },
+    getOrderCount() {
+      var count = 0;
+      for (var cnt of this.order.products) {
+        if (cnt) {
+          count += cnt;
+        }
+      }
+      return count;
+    },
+    getOrderTotal() {
+      var total = 0;
+      for (let index = 0; index < this.order.products.length; ++index) {
+        if (!this.order.products[index]) {
+          continue;
+        }
+        total += this.order.products[index] * (this.getProductById(index)).price;
+      }
+      return total;
+    },
+    getProductById(productId) {
+      for (let product of this.data.products) {
+        if (productId === product.id) {
+          return product;
+        }
+      }
     }
   }
 }
@@ -73,12 +135,16 @@ export default {
 
 <style>
 
+div {
+  border: 0px solid yellow;
+}
+
 .product {
   display: flex;
   max-width: 24rem;
   margin: 0 auto;
   padding: 1rem;
-  border-radius: 0.5rem;
+  border-top: 1px solid grey;
   background-color: #000;
 }
 .product:hover {
@@ -94,21 +160,45 @@ export default {
 .product-content {
   margin-left: 1.5rem;
   padding-top: 0.25rem;
-
+  max-width: 17rem;
+  width: 100%;
+  flex-shrink: 0;
 }
+
 .product-name {
-  display: flex;
+  display: inline;
+  text-align: left;
+  float: left;
   font-size: 1.25rem;
   line-height: 1.25;
   color: #fff;
 }
+.product-price {
+  float: right;
+  font-size: 1.25rem;
+  line-height: 1.25;
+  color: #fff;
+  font-weight: bold;
+}
 .product-description {
-  display: flex;
+  display: inline;
+  text-align: left;
+  float: left;
   font-size: 1rem;
   line-height: 1.5;
   color: #bbb;
+  max-width: 12rem;
 }
-
+.main {
+  display: flex;
+  max-width: 24rem;
+  margin: 0 auto;
+  background-color: #000;
+}
+.categories {
+  float: left;
+  min-width: 20rem;
+}
 .category-button {
   display: inline-flex;
   max-width: 24rem;
@@ -124,5 +214,39 @@ export default {
   color: #fff;
   font-weight: bold;
 }
+
+.product-order {
+  display: inline;
+  float: right;
+}
+.order-hidden {
+  display: none;
+}
+.order-count {
+  font-weight: bold;
+  font-size: 20px;
+  color: #880000;
+  border: 1px solid #880000;
+  background-color: #ff6666;
+  border-radius: 10px;
+  float: right;
+  text-align: center;
+}
+
+.product-minus, .product-plus {
+  font-size: 30px;
+  color: red;
+  font-weight: bold;
+  padding-left: 0.3rem;
+  padding-right: 0.3rem;
+  border: 0;
+}
+
+.product-count {
+  font-size: 20px;
+  width: 2rem;
+  background-color: #fff;
+}
+
 
 </style>
